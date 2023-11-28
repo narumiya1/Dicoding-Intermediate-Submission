df7c8cea90d27b3c445edc6d64d047a44d7becb9
narumiya1 <narumiiyamei@gmail.com>
2023-11-28 20:27:02 +0700

    Register apiservice

--- app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
@@ -1,6 +1,8 @@
 package com.ackerman.intermediatesubmission.data.remote.api
 
 import com.ackerman.intermediatesubmission.data.remote.response.LoginRequest
+import com.ackerman.intermediatesubmission.data.remote.response.RegisterRequest
+import com.ackerman.intermediatesubmission.data.remote.response.RegisterResponse
 import com.ackerman.intermediatesubmission.data.remote.response.ResponseLogin
 import retrofit2.http.*
 
@@ -11,6 +13,9 @@ interface ApiService {
         @Body request: LoginRequest
     ): ResponseLogin
 
-
+    @POST("register")
+    suspend fun userRegister(
+        @Body request: RegisterRequest
+    ): RegisterResponse
 
 }
