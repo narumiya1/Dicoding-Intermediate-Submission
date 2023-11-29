6eb60a9f9a4eb64025c313588ae37f282565ee62
narumiya1 <narumiiyamei@gmail.com>
2023-11-29 14:47:20 +0700

    Data Story get' Injection

--- app/src/main/java/com/ackerman/intermediatesubmission/data/di/Injection.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/di/Injection.kt
@@ -6,15 +6,15 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.ackerman.intermediatesubmission.data.local.UserPreference
 import com.ackerman.intermediatesubmission.data.remote.api.ApiConfig
-import com.ackerman.intermediatesubmission.data.repository.StoryaRepository
+import com.ackerman.intermediatesubmission.data.repository.DataStoryRepository
 
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore("storyApp")
 
 object Injection {
-    fun provideRepository(context: Context) : StoryaRepository{
+    fun provideRepository(context: Context) : DataStoryRepository{
         val preference = UserPreference.getInstance(context.dataStore)
         val apiService = ApiConfig.getApiService()
-        return StoryaRepository.getInstance(preference, apiService)
+        return DataStoryRepository.getInstance(preference, apiService)
     }
 }
