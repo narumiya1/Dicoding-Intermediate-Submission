4475a1a145a4d8db397f3d6782fcb3ef6f75adfe
narumiya1 <narumiiyamei@gmail.com>
2023-11-29 14:46:49 +0700

    Data Story get ViewModelFactory

--- app/src/main/java/com/ackerman/intermediatesubmission/data/utils/ViewModelFactory.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/utils/ViewModelFactory.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.ackerman.intermediatesubmission.data.di.Injection
-import com.ackerman.intermediatesubmission.data.repository.StoryaRepository
+import com.ackerman.intermediatesubmission.data.repository.DataStoryRepository
 import com.ackerman.intermediatesubmission.data.view_ui.auth.LoginViewModel
 import com.ackerman.intermediatesubmission.data.view_ui.auth.RegisterViewModel
+import com.ackerman.intermediatesubmission.data.view_ui.story.StoryViewModel
 
-class ViewModelFactory (private val repository: StoryaRepository) : ViewModelProvider.NewInstanceFactory() {
+class ViewModelFactory (private val repository: DataStoryRepository) : ViewModelProvider.NewInstanceFactory() {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -18,6 +19,10 @@ class ViewModelFactory (private val repository: StoryaRepository) : ViewModelPro
         if (modelClass.isAssignableFrom(RegisterViewModel::class.java)) {
             return RegisterViewModel(repository) as T
         }
+        if (modelClass.isAssignableFrom(StoryViewModel::class.java)) {
+            return StoryViewModel(repository) as T
+        }
+
         throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
     }
 
