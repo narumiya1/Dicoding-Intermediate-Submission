21b0ac04a269b7a9c425716e7e03766d2933534e
narumiya1 <narumiiyamei@gmail.com>
2023-11-29 14:57:23 +0700

    UI Story rename Repository

--- app/src/main/java/com/ackerman/intermediatesubmission/data/view_ui/auth/LoginViewModel.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/view_ui/auth/LoginViewModel.kt
@@ -3,22 +3,22 @@ package com.ackerman.intermediatesubmission.data.view_ui.auth
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ackerman.intermediatesubmission.data.local.UserModel
-import com.ackerman.intermediatesubmission.data.repository.StoryaRepository
+import com.ackerman.intermediatesubmission.data.repository.DataStoryRepository
 import kotlinx.coroutines.launch
 
-class LoginViewModel(private val storyaRepository: StoryaRepository) : ViewModel() {
+class LoginViewModel(private val dataStoryRepository: DataStoryRepository) : ViewModel() {
 
-    fun loginUser(email: String, password: String) = storyaRepository.loginUser(email, password)
+    fun loginUser(email: String, password: String) = dataStoryRepository.loginUser(email, password)
 
     fun saveUser(userModel: UserModel){
         viewModelScope.launch {
-            storyaRepository.saveDataUser(userModel)
+            dataStoryRepository.saveDataUser(userModel)
         }
     }
 
     fun logout() {
         viewModelScope.launch {
-            storyaRepository.logout()
+            dataStoryRepository.logout()
         }
     }
 }
--- app/src/main/java/com/ackerman/intermediatesubmission/data/view_ui/auth/RegisterViewModel.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/view_ui/auth/RegisterViewModel.kt
@@ -1,9 +1,9 @@
 package com.ackerman.intermediatesubmission.data.view_ui.auth
 
 import androidx.lifecycle.ViewModel
-import com.ackerman.intermediatesubmission.data.repository.StoryaRepository
+import com.ackerman.intermediatesubmission.data.repository.DataStoryRepository
 
-class RegisterViewModel (private val storyRepository: StoryaRepository): ViewModel() {
+class RegisterViewModel (private val storyRepository: DataStoryRepository): ViewModel() {
 
     fun userRegister(name: String, email: String, password: String) =
         storyRepository.registerUser(name, email, password)
