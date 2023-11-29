8f42304844c976bafbda5951d4f808b032a6ebfa
narumiya1 <narumiiyamei@gmail.com>
2023-11-29 14:43:29 +0700

    Data Preference getUserData userModel

--- app/src/main/java/com/ackerman/intermediatesubmission/data/local/UserPreference.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/local/UserPreference.kt
@@ -5,6 +5,8 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.map
+import java.util.concurrent.Flow
 
 class UserPreference private constructor(private val dataStore : DataStore<Preferences>) {
 
@@ -25,6 +27,15 @@ class UserPreference private constructor(private val dataStore : DataStore<Prefe
     suspend fun logout() {
         dataStore.edit { preferences ->
             preferences.clear()
+        }
+    }
+    fun getUserData(): kotlinx.coroutines.flow.Flow<UserModel> {
+        return dataStore.data.map { preferences ->
+            UserModel(
+                preferences[NAME] ?: "",
+                preferences[TOKEN] ?: "",
+                preferences[STATE] ?: false
+            )
         }
     }
 
