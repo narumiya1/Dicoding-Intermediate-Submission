60a354769de7d02532ab565f8c6e157d22a1cfa8
narumiya1 <narumiiyamei@gmail.com>
2023-11-30 08:26:14 +0700

    Post Story ViewModel

---
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/view_ui/story/PostStoryViewModel.kt
@@ -0,0 +1,18 @@
+package com.ackerman.intermediatesubmission.data.view_ui.story
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.ackerman.intermediatesubmission.data.local.UserModel
+import com.ackerman.intermediatesubmission.data.repository.DataStoryRepository
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+
+class PostStoryViewModel(private val storyRepository: DataStoryRepository): ViewModel() {
+
+    fun postStory(token: String, file: MultipartBody.Part, desc : RequestBody) =
+        storyRepository.postStory(token, file, desc)
+
+    fun getUser(): LiveData<UserModel> {
+        return storyRepository.getCurrentUserData()
+    }
+}
