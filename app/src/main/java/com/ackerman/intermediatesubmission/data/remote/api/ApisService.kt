45ac55b487b7d4e5a6b94779dc3178130026c019
narumiya1 <narumiiyamei@gmail.com>
2023-11-30 08:24:11 +0700

    Post Story url service

--- app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
@@ -1,6 +1,8 @@
 package com.ackerman.intermediatesubmission.data.remote.api
 
 import com.ackerman.intermediatesubmission.data.remote.response.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface ApiService {
@@ -22,4 +24,11 @@ interface ApiService {
         @Query("size") size: Int
     ): StoryResponse
 
+    @Multipart
+    @POST("stories")
+    suspend fun addStory(
+        @Header("Authorization") token: String,
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody
+    ): PostStoryResponse
 }
