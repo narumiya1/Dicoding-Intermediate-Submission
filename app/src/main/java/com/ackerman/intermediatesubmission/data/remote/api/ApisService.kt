bf5bb7140e6ad52ddca59ce8e51aad6d588b8e0d
narumiya1 <narumiiyamei@gmail.com>
2023-12-04 20:22:40 +0700

    Location Maps Apiservice story location

--- app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
+++ app/src/main/java/com/ackerman/intermediatesubmission/data/remote/api/ApisService.kt
@@ -24,11 +24,19 @@ interface ApiService {
         @Query("size") size: Int
     ): StoryResponse
 
+    @GET("stories")
+    suspend fun getStoryLocation(
+        @Header("Authorization") token: String,
+        @Query("location") location: Int = 1,
+    ): StoryResponse
+    
     @Multipart
     @POST("stories")
     suspend fun addStory(
         @Header("Authorization") token: String,
         @Part file: MultipartBody.Part,
-        @Part("description") description: RequestBody
+        @Part("description") description: RequestBody,
+        @Part("lat") lat: Double?,
+        @Part("lon") lon: Double?
     ): PostStoryResponse
 }
